0a856ffb41532a7335fc993455f44ed69641d7e0
pirodev <pirofiredev@gmail.com>
2026-07-13 22:21:18 +0200

    extensions done, overall tweaks

--- src/app/expanded/[id]/page.tsx
+++ src/app/expanded/[id]/page.tsx
@@ -1,22 +1,29 @@
 import Header from "../../../components/bridge/headerBridge";
-import ExpandedCard from "../../../components/client/templates/expandedCardTemplate";
-import {Suspense} from "react";
+import ExpandedCardBridge from "../../../components/bridge/expandedCardBridge";
+import { Suspense } from "react";
 import LoadingAnimation from "../../../components/client/templates/layout/loadingAnimation";
-
 
 type PageProps = {
     params: Promise<{ id: string }>;
 };
 
-export default async function Expanded({ params }: PageProps) {
+// done trick here by declaring two separate function, because params were interrupting <Suspend> react flow
+
+async function ExpandedContent({ params }: PageProps) {
     const { id } = await params;
 
-    console.log(id);
-
     return (
-        <Suspense fallback={<LoadingAnimation/>}>
+        <>
             <Header />
-            <ExpandedCard/>
+            <ExpandedCardBridge postId={parseInt(id, 10)} />
+        </>
+    );
+}
+
+export default function Expanded({ params }: PageProps) {
+    return (
+        <Suspense fallback={<LoadingAnimation />}>
+            <ExpandedContent params={params} />
         </Suspense>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
-'use server'
 import Header from "../components/bridge/headerBridge";
 import PostBridge from "../components/bridge/postBridge";
 import Footer from "../components/client/templates/layout/footerTemplate";
-import {Suspense} from "react";
+import { Suspense } from "react";
 import LoadingAnimation from "../components/client/templates/layout/loadingAnimation";
 
-
-export default async function Feed() {
-  return (
-      <>
-        <Suspense fallback={<LoadingAnimation/>}>
-            <Header />
-            <PostBridge/>
-            <Footer/>
-        </Suspense>
-      </>
-  );
+export default function Feed() {
+    return (
+            <Suspense fallback={<LoadingAnimation />}>
+                <Header />
+                <PostBridge />
+                <Footer />
+            </Suspense>
+    );
 }
